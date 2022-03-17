315a8f933167a7ccf9765503fdaba68b17d83054
Sandeep Bhatula <sandeepbvv11@users.noreply.github.com>
2022-03-16 23:39:23 -0700

    Created an application of tictactoe
    Created an application of tictactoe.Checked for winning combination,check for tie is done.

---
+++ src/Game.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Board } from "./Board";
+import "./Game.css";
+import { useState } from "react";
+import { ResultModal } from "./ResultModal";
+import { calculateWinner } from "./WinnerCalculater";
+export const Game = ({value}) => {
+  const [cellValues,setCellValues]=useState(["","","","","","","","",""]);
+  const [xIsNext,setXIsNext]=useState(true);
+  const [gameOver,setgameOver]=useState(false);
+  const winningCombination=[];
+  const isCellEmpty=(cellIndex)=>cellValues[cellIndex]==="";
+  const [numberOfTurnsLeft,setNumberOfTurnsLeft]=useState(9);
+  const cellClicked=(cellIndex)=>{
+    if(isCellEmpty(cellIndex)){
+      
+    const newCellValues=[...cellValues];
+    newCellValues[cellIndex]=xIsNext?"X":"O";
+    const newNumberOfTurnsLeft= numberOfTurnsLeft-1;
+    //Calculate result
+    const calcResult=calculateWinner(newCellValues,newNumberOfTurnsLeft,cellIndex);
+    setXIsNext(!xIsNext);
+    setCellValues(newCellValues);
+    setgameOver(calcResult.hasResult);
+    setNumberOfTurnsLeft(newNumberOfTurnsLeft);
+    }
+    //console.log('Cell '+ cellIndex+ ' clicked')
+  }
+  return (
+    <>
+      <div id="game">
+        <h1>Tic Tac Toe</h1>
+        <Board cellValues={cellValues} winningCombination={winningCombination} cellClicked={cellClicked}/>
+      </div>
+      <ResultModal gameOver={gameOver}/>
+      
+    </>
+  );
+};
---
+++ src/ResultModal.js
@@ -0,0 +1,29 @@
+import classNames from "classnames";
+import React from "react";
+import "./ResultModal.css";
+
+export const ResultModal = (props) => {
+  const resultModalClasses=classNames({
+      'modal-open':props.gameOver
+  })
+ 
+  return (
+    <>
+    
+    <div id="modal-overlay" className={resultModalClasses}>
+        <div id="game-result-modal">
+          <div id="result-container">
+            <div id="winner-container">
+              <span></span>
+            </div>
+          </div>
+          <div id="new-game-container">
+            <button id="new-game-button">Start New Game</button>
+          </div>
+        </div>
+      </div>
+         
+        
+    </>
+  );
+}
